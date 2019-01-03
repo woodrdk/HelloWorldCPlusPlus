d5492f73ed032bbcab47e26ef96281719a34c716
Robert Wood Jr <rdrwood@gmail.com>
2019-01-03 13:39:25 -0800

    first c++ program in class and added some extra text

---
+++ HelloWorld/Main.cpp
@@ -0,0 +1,19 @@
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+	string str = "hello, World!";
+	string rob = "This is Rob's first C++ program!";
+	string ending = "Exciting Huh?";
+	
+	cout << str << endl;
+	cout << rob << endl;
+	cout << ending << endl;
+	cout << endl;
+	system("pause");
+	return 0;
+}
